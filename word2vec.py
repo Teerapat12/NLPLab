ed800104efebc7dbf63ea0e1aa8c8f826e287b1c
Teerapat12 <Teerapat12@users.noreply.github.com>
2017-04-26 12:57:22 +0700

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -1,61 +1,47 @@
+# -*- coding: utf-8 -*-
+
 
 import pandas as pd
 
-dataset = pd.read_csv("train.csv")
+dataset = pd.read_excel("pokemon TW TH search.xlsx",skiprows=1,encoding ='utf-8')
 
 from bs4 import BeautifulSoup
 import re
 from nltk.corpus import stopwords
 
-def review_to_wordlist( question, remove_stopwords=False ):
+from pythainlp.segment import segment
+
+#For strip accents
+import unicodedata
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
+
+def review_to_wordlist(review):
+    review_text = review
+    review_text = review_text.lower()
+    review_text = review_text.replace(u'é','e')
+    pokemonSyn = [u'pokemongo',u'โปเกม่อนโก',u'โปเกมอนโก',u'pokemon go']
+    for syn in pokemonSyn: review_text = review_text.replace(syn,u'ม่อน')
+    removeCharList = ['rt','!','?']
+    for c in removeCharList: review_text = review_text.replace(c,'')
+    elimSet = ['http','@','#']
+    review_word_list = review_text.split()
+    for e in elimSet:
+        review_word_list = [word for word in review_word_list if e not in word]
+        
+    reviewSentence = "".join(review_word_list) 
+    wordList = segment(reviewSentence)
     
-    question_text = re.sub("[^a-zA-Z]"," ", question)
-    #
-    # 3. Convert words to lower case and split them
-    words = question_text.lower().split()
-    #
-    # 4. Optionally remove stop words (false by default)
-    if remove_stopwords:
-        stops = set(stopwords.words("english"))
-        words = [w for w in words if not w in stops]
-    #
-    # 5. Return a list of words
-    return(words)
+    return wordList
 
-import nltk.data
-nltk.data.path.append("D:\nltk_data")
-#nltk.download('all', halt_on_error=False)   
-
-# Load the punkt tokenizer
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
-def review_to_sentences( question, tokenizer, remove_stopwords=False ):
-    # Function to split a review into parsed sentences. Returns a 
-    # list of sentences, where each sentence is a list of words
-    #
-    # 1. Use the NLTK tokenizer to split the paragraph into sentences
-    raw_sentences = tokenizer.tokenize(question.strip())
-    #
-    # 2. Loop over each sentence
-    sentences = []
-    for raw_sentence in raw_sentences:
-        # If a sentence is empty, skip it
-        if len(raw_sentence) > 0:
-            # Otherwise, call review_to_wordlist to get a list of words
-            sentences.append( review_to_wordlist( raw_sentence, \
-              remove_stopwords ))
-    #
-    # Return the list of sentences (each sentence is a list of words,
-    # so this returns a list of lists
-    return sentences
 
 sentences = []  # Initialize an empty list of sentences
-print("Parsing sentences from dataset q1")
-for question in dataset["question1"]:
-    sentences += review_to_sentences(str(question), tokenizer)
-print("Parsing sentences from dataset q2")    
-for question in dataset["question2"]:
-    sentences += review_to_sentences(str(question), tokenizer)    
+print("Parsing Tweet Text sentences from dataset ")
+for row in dataset["Tweet Text"].head(5):
+    print(row)
+    print(type(review_to_wordlist(row)))
+    sentences.append(review_to_wordlist(row))
 
 
 # Import the built-in logging module and configure it so that Word2Vec 
@@ -86,4 +72,3 @@ model.init_sims(replace=True)
 # save the model for later use. You can load it later using Word2Vec.load()
 model_name = "quora_context"
 model.save(model_name)
-
