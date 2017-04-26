5ec2f0b4c8f0bf8469e93785d83f874309ed5682
Teerapat12 <Teerapat12@users.noreply.github.com>
2017-04-26 08:21:21 +0700

    Create word2vec.py

---
+++ word2vec.py
@@ -0,0 +1,79 @@
+
+import pandas as pd
+
+dataset = pd.read_csv("train.csv")
+
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+
+def review_to_wordlist( question, remove_stopwords=False ):
+    
+    question_text = re.sub("[^a-zA-Z]"," ", question)
+    #
+    # 3. Convert words to lower case and split them
+    words = question_text.lower().split()
+    #
+    # 4. Optionally remove stop words (false by default)
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
+    #
+    # 5. Return a list of words
+    return(words)
+
+import nltk.data
+nltk.data.path.append("D:\nltk_data")
+#nltk.download('all', halt_on_error=False)   
+
+# Load the punkt tokenizer
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+def review_to_sentences( question, tokenizer, remove_stopwords=False ):
+    # Function to split a review into parsed sentences. Returns a 
+    # list of sentences, where each sentence is a list of words
+    #
+    # 1. Use the NLTK tokenizer to split the paragraph into sentences
+    raw_sentences = tokenizer.tokenize(question.strip())
+    #
+    # 2. Loop over each sentence
+    sentences = []
+    for raw_sentence in raw_sentences:
+        # If a sentence is empty, skip it
+        if len(raw_sentence) > 0:
+            # Otherwise, call review_to_wordlist to get a list of words
+            sentences.append( review_to_wordlist( raw_sentence, \
+              remove_stopwords ))
+    #
+    # Return the list of sentences (each sentence is a list of words,
+    # so this returns a list of lists
+    return sentences
+
+sentences = []  # Initialize an empty list of sentences
+print("Parsing sentences from dataset q1")
+for question in dataset["question1"]:
+    sentences += review_to_sentences(str(question), tokenizer)
+print("Parsing sentences from dataset q2")    
+for question in dataset["question2"]:
+    sentences += review_to_sentences(str(question), tokenizer)    
+
+
+# Import the built-in logging module and configure it so that Word2Vec 
+# creates nice output messages
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+    level=logging.INFO)
+
+# Set values for various parameters
+num_features = 300    # Word vector dimensionality                      
+min_word_count = 40   # Minimum word count                        
+num_workers = 4       # Number of threads to run in parallel
+context = 10          # Context window size                                                                                    
+downsampling = 1e-3   # Downsample setting for frequent words
+
+# Initialize and train the model (this will take some time)
+from gensim.models import word2vec
+print("Training model...")
+model = word2vec.Word2Vec(sentences, workers=num_workers, \
+            size=num_features, min_count = min_word_count, \
+            window = context, sample = downsampling)
